5d24d679e77610c0cdf5047a795e620c3644346b
Han Jinseok <moregorenine@gmail.com>
2019-12-06 00:27:04 +0900

    Initial commit

---
+++ src/main/java/com/w4springrain/showmethedrawing/ShowMeTheDrawingApplication.java
@@ -0,0 +1,13 @@
+package com.w4springrain.showmethedrawing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowMeTheDrawingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowMeTheDrawingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/w4springrain/showmethedrawing/ShowMeTheDrawingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w4springrain.showmethedrawing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowMeTheDrawingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
